2590972a93f61f7685293b2930ea950d5cde3990
Eleuterius <eleuteriusmmuru77@gmail.com>
2026-03-13 20:27:13 +0300

    Add scroll animations and portfolio section

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const sidebar = document.querySelector(".sidebar");
 menu.addEventListener("click", () => {
     sidebar.classList.toggle("active");
 });
+
+// Scroll reveal
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    const trigger = window.innerHeight * 0.8;
+
+    sections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+
+        if(top < trigger){
+            section.classList.add("show");
+        }
+    });
+});
